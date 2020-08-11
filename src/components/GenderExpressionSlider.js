bfa29d4dff4dfc705eadeeedb1edf91e6ba4022d
holdenmad <holden.madagame@gmail.com>
2020-08-11 14:50:28 +0200

    will delete

--- src/components/GenderExpressionSlider.js
+++ src/components/GenderExpressionSlider.js
@@ -22,27 +22,37 @@ const genderExpMatrix = {
   butch: {
     min: { femininity: 0, masculinity: 80 },
     max: { femininity: 20, masculinity: 100 },
-    description: "Description of Butch",
+    description: "Description of Butch"
   },
   femme: {
     min: { femininity: 80, masculinity: 0 },
     max: { femininity: 100, masculinity: 20 },
+    description: "Description of femme"
+
   },
   androgynous: {
     min: { femininity: 40, masculinity: 40 },
     max: { femininity: 70, masculinity: 70 },
+    description: "Description of andrygynous"
+
   },
   nonbinary: {
     min: { femininity: 10, masculinity: 10 },
     max: { femininity: 90, masculinity: 90 },
+    description: "Description of nonbinary"
+
   },
   genderfluid: {
     min: { femininity: 0, masculinity: 0 },
     max: { femininity: 100, masculinity: 100 },
+    description: "Description of genderfluid"
+
   },
   agender: {
     min: { femininity: 0, masculinity: 0 },
     max: { femininity: 20, masculinity: 20 },
+    description: "Description of agender"
+
   },
 };
 
@@ -57,6 +67,8 @@ const GenderExpressionSlider = () => {
   const computeVal = () => {
     const f = genderexp1Ref.current.value;
     const m = genderexp2Ref.current.value;
+
+    //change this so you filter the whole object rather than just the key
     const matchingExpressions = Object.keys(genderExpMatrix).filter((key) => {
       key = genderExpMatrix[key];
       console.log("coords:", f, m, key);
@@ -67,6 +79,7 @@ const GenderExpressionSlider = () => {
         m <= key.max.masculinity
       );
     });
+    console.log(matchingExpressions);
     setGenderExp(matchingExpressions);
   };
 
@@ -99,7 +112,7 @@ const GenderExpressionSlider = () => {
           min="0"
           max="100"
           defaultValue="50"
-          step="0.1"
+          step={1}
           className="slider"
           label="Feminine Expression Slider"
         />
@@ -113,7 +126,7 @@ const GenderExpressionSlider = () => {
           min="0"
           max="100"
           defaultValue="50"
-          step="0.1"
+          step={1}
           className="slider"
           label="Masculine Expression Slider"
         />
